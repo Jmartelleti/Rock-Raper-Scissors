0d7cfb5a92f8f2c8da431676bdbe34052ed48d6d
juan cruz martelleti <juanmartelleti@gmail.com>
2023-01-16 00:32:43 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+Version programada en python del clasico juago piedra papel o tijeras. 
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+Version programmed in python of the classic rock-paper-scissors game.
---
+++ rock_paper_scissors.py
@@ -0,0 +1,69 @@
+# CLASIC GAME  ROCK, PAPER AND SCISSORS 
+import random
+options = ('piedra', 'papel', 'tijera')
+#RECOLECCION DE DATOS  Y SELECCION DE DATOS POR PARTE DE PC// data entry by user and computer 
+def choose_options(): 
+    user_option = input('piedra, papel o tijera => ')
+    user_option = user_option.lower()
+    if not user_option in options:
+        print('esa opcion no es valida')
+        #continue
+        return None , None
+    computer_option = random.choice(options)
+    print('User option =>', user_option)
+    print('Computer option =>', computer_option)
+    return user_option,computer_option # returns user and pc election option
+
+def check_rules(user_option, computer_option,user_wins,computer_wins): #game logic for checking options
+    if user_option == computer_option:
+        print('Empate!')
+    elif user_option == 'piedra':
+        if computer_option == 'tijera':
+            print('piedra gana a tijera')
+            print('user gano!')
+            user_wins += 1
+        else:
+            print('Papel gana a piedra')
+            print('computer gano!')
+            computer_wins += 1
+    elif user_option == 'papel':
+        if computer_option == 'piedra':
+            print('papel gana a piedra')
+            print('user gano')
+            user_wins += 1
+        else:
+            print('tijera gana a papel')
+            print('computer gano!')
+            computer_wins += 1
+    elif user_option == 'tijera':
+        if computer_option == 'papel':
+            print('tijera gana a papel')
+            print('user gano!')
+            user_wins += 1
+        else:
+            print('piedra gana a tijera')
+            print('computer gano!')
+            computer_wins += 1
+    return user_wins,computer_wins
+
+def run_game():  #main game loop 
+    computer_wins = 0
+    user_wins = 0
+    rounds = 1
+    while True:
+        print('*' * 10)
+        print('ROUND', rounds)
+        print('*' * 10)
+        print('computer_wins', computer_wins)
+        print('user_wins', user_wins)
+        rounds += 1 
+        user_option, computer_option = choose_options()
+        user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+        if computer_wins == 2:
+            print('El ganador es la computadora')
+            break
+        if user_wins == 2:
+            print('El ganador es el usuario')
+            break
+
+run_game() # excecute main function game
